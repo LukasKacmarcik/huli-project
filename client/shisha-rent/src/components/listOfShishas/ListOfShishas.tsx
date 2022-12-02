f3bc65a1b76efa61f57decac7f51f056df502618
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-12-02 16:26:00 +0100

    add dummy shishas and basic style

--- client/shisha-rent/src/components/listOfShishas/ListOfShishas.tsx
+++ client/shisha-rent/src/components/listOfShishas/ListOfShishas.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import ShishaComponent from "../shisha/Shisha";
 import { fetchShishas, Shisha } from "../../app/slices/shishas";
+import styles from "./ListOfShishas.module.scss";
 
 const ListOfShishas: React.FC = () => {
   const shishas: Shisha[] = useAppSelector((state) => state.shishas.shishas);
@@ -20,7 +21,7 @@ const ListOfShishas: React.FC = () => {
     console.log("ListOfShishas useEffect run");
   }, [dispatch]);
 
-  return <ul>{listOfShishas}</ul>;
+  return <ul className={styles.listOfShisha}>{listOfShishas}</ul>;
 };
 
 export default ListOfShishas;
--- client/shisha-rent/src/components/shisha/Shisha.tsx
+++ client/shisha-rent/src/components/shisha/Shisha.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Shisha as ShishaType } from "../../app/slices/shishas";
+import styles from "./Shisha.module.scss";
 
 interface Props {
   passedShisha: ShishaType;
 }
 
 const Shisha: React.FC<Props> = ({ passedShisha }) => {
-  // passedShisha.
+  console.log(passedShisha);
+
   return (
-    <>
-      <img src={passedShisha.selectedFile} alt="shisha" />
+    <div className={styles.shisha}>
+      <div className={styles.imgWrapper}>
+        <img src={passedShisha.selectedFile} alt="shisha" />
+      </div>
       <h2>{passedShisha.name}</h2>
       <p>{passedShisha.description}</p>
       <h4>{passedShisha.price}</h4>
-    </>
+    </div>
   );
 };
 
