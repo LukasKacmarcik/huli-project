2155620cd1f854dfb1c861da96181d685556a4e9
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-12-06 17:35:10 +0100

    implement DatePicker and its basic functionality

--- client/shisha-rent/global.d.ts
+++ client/shisha-rent/global.d.ts
@@ -1,2 +1,3 @@
 ////this didnt work with fixing import issue without types for this module but its here just to be safe
 declare module "react-file-base64;";
+declare module "react-datepicker";
--- client/shisha-rent/src/components/shisha/selectedShisha/shishasDatePicker/ShishasDatePicker.tsx
+++ client/shisha-rent/src/components/shisha/selectedShisha/shishasDatePicker/ShishasDatePicker.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import styles from "./ShishasDatePicker.module.scss";
+import { useState } from "react";
+//// Solution from https://bobbyhadz.com/blog/typescript-could-not-find-a-declaration-file-for-module-react
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import subDays from "date-fns/subDays";
 
 const ShishasDatePicker: React.FC = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const myTime = new Date("2022-12-26T16:00:48.212Z");
+  const myDatesArr: Date[] = [myTime, subDays(myTime, -1)];
+
   return (
     <div className={styles.shishasDatePicker}>
       <h1>ShishasDatePicker</h1>
+      <DatePicker
+        selected={startDate}
+        onChange={(date: Date) => setStartDate(date)}
+        minDate={subDays(new Date(), -1)}
+        excludeDates={myDatesArr}
+        placeholderText="Select a date other than today or yesterday"
+      />
     </div>
   );
 };
