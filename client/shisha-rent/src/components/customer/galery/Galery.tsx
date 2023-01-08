f7c144aa67a8dc8da288ed1020cf47266d7c2f8f
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2023-01-08 16:02:17 +0100

    populate customer home

---
+++ client/shisha-rent/src/components/customer/galery/Galery.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Galery.module.scss";
+
+const Galery = () => {
+  return <div className={styles.galeryWrapper}>Galery</div>;
+};
+
+export default Galery;
--- client/shisha-rent/src/components/customer/home/Home.tsx
+++ client/shisha-rent/src/components/customer/home/Home.tsx
@@ -1,6 +1,10 @@
 import { useAppSelector } from "../../../app/hooks";
 import ListOfShishas from "../listOfShishas/ListOfShishas";
 import SelectedShisha from "../shisha/selectedShisha/SelectedShisha";
+import logo from "../../../images/logo-biela.png";
+import rentImg from "../../../images/box.png";
+import styles from "./Home.module.scss";
+import Galery from "../galery/Galery";
 
 export default function Home() {
   const selectedShisha = useAppSelector(
@@ -9,7 +13,38 @@ export default function Home() {
 
   return (
     <>
-      <div style={{ maxWidth: "100vw", minHeight: "100vh" }}></div>
+      <div className={styles.hero}>
+        <div className={styles.logoWrapper}>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className={styles.aboutUsWrapper}>
+          <h1>O NÁS</h1>
+          <p>
+            Čajovňa sa nachádza v historickom centre mesta Kežmarok, so
+            zachovanými historickymi prvkami a príjemným prostredím. Pripravíme
+            Vám skvelý čaj, výbornú belgickú čokoládu alebo vodnú fajku podľa
+            chuti.
+          </p>
+          <p>
+            Počas letných dní Vás radi privítame aj na našej záhradnej terase.
+          </p>
+        </div>
+        <Galery />
+        <div className={styles.rentWrapper}>
+          <h1>PRENÁJOM VODNEJ FAJKY</h1>
+          <div className={styles.rentImgWrapper}>
+            <img src={rentImg} alt="Shisha rent" />
+          </div>
+          <p>
+            Dovezieme Vám Vami vybraný typ vodnej fajky so všetkým potrebným
+            príslušenstvom priamo k Vašim dverám. Už bude treba len rozpáliť
+            uhlíky a môžete si užiť session v pohodlí domova. Počas jedného
+            nájmu (do 24 hod.) môžete vodnú fajku využiť hneď niekoľkokrát.
+            Môžete si doobjednať nápoje, snacky, extra korunky s príchuťami,
+            poprípade iné zaujímavé doplnky
+          </p>
+        </div>
+      </div>
       <ListOfShishas />
       {selectedShisha ? <SelectedShisha /> : null}
     </>
