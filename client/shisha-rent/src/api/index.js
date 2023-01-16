4ac6fc6bbca6a2f77cda1924c78b2789a3fd8653
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2023-01-16 22:04:45 +0100

    update so hosting will work

--- client/shisha-rent/src/api/index.js
+++ client/shisha-rent/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:3001/api";
+const url = process.env.PRODUCTION_URL || "http://localhost:3001/api";
 
 export const api = axios.create({
   baseURL: url,
