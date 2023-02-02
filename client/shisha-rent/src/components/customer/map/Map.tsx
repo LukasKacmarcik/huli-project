87a3b2dce685a826c872451aabdc831b2240ed07
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2023-02-02 16:20:22 +0100

    redo map to show image and refrence google map so iframe does not collect cookies

--- client/shisha-rent/src/components/customer/map/Map.tsx
+++ client/shisha-rent/src/components/customer/map/Map.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styles from "./Map.module.scss";
+import fakeMap from "../../../images/fakeMap2.png";
 
 const Map = () => {
-  //// This map is causing 168 issues in console
+  //// Iframe is collecting cookies and for that we would need some cookie consent from user.
   return (
-    <iframe
-      title="Map"
+    // <iframe
+    //   title="Map"
+    //   className={styles.map}
+    //   src="https://maps.google.com/maps?q=cajovna%20aura%20kezmarok&t=&z=17&ie=UTF8&iwloc=&output=embed"
+    // ></iframe>
+
+    <a
       className={styles.map}
-      src="https://maps.google.com/maps?q=cajovna%20aura%20kezmarok&t=&z=17&ie=UTF8&iwloc=&output=embed"
-    ></iframe>
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.google.com/maps/place/%C4%8Cajov%C5%88a+AurA+Ke%C5%BEmarok/@49.139452,20.431984,17z/data=!4m5!3m4!1s0x0:0x47e430b22c1ae443!8m2!3d49.1393765!4d20.4320678?hl=en-US"
+    >
+      <img src={fakeMap} alt="mapa"></img>
+    </a>
   );
 };
 
